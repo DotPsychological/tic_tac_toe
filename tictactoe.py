66060507d7d129f4aae2415fa6c8e2a4ce529135
Yobel <yobelnathalie@gmail.com>
2021-09-18 21:39:55 +0900

    Added a visual reference for the game board and dictionary for each collumns

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,4 @@
-# TicTacToe Game
+# Game title and Rules
 
 print(""" 
   _______ _        _______           _______         
@@ -9,3 +9,20 @@ print("""
     |_|  |_|\___|    |_|\__,_|\___|    |_|\___/ \___|
                                                                                                          
 """)
+
+print("""Game Overview
+Tic Tac Toe is a game that requires two players. One will be X and one will be O.
+The objective is draw a line, either horizontal, vertical, or diagonally in a 3x3 before the other player does. X will be starting first in this game \n"""
+)
+
+# Main Board
+print("A1 | A2 | A3")
+print("———|————|————")
+print("B1 | B2 | B3")
+print("———|————|————")
+print("C1 | C2 | C3")
+
+# Dictionaries for each line
+line1 = {"A1": "NULL", "A2": "NULL", "A3": "Null"}
+line2 = {"B1": "NULL", "B2": "NULL", "B3": "Null"}
+line3 = {"B1": "NULL", "B2": "NULL", "B3": "Null"}
